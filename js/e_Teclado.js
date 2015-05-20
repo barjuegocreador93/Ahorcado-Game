5951d548d9350a685d6e96b7cbb9ef7a0fad9b62
barjuegocreador93 <barbosa93cg@gmail.com>
2015-05-20 15:43:49 -0500

    Ahorcado-Game 1.02
    Se a modificado el evento del teclado del juego

---
+++ js/e_Teclado.js
@@ -0,0 +1,86 @@
+/// <reference path="../typings/jquery/jquery.d.ts"/>
+/***Hecho por camilo barbosa */
+
+
+$(document).ready(function()
+{
+	
+	$("#A").click(function(){
+			estar("A");		
+	});
+	$("#B").click(function(){
+			estar("B");		
+	});
+	$("#C").click(function(){
+			estar("C");		
+	});
+	$("#D").click(function(){
+			estar("D");		
+	});
+	$("#E").click(function(){
+			estar("E");		
+	});
+	$("#F").click(function(){
+			estar("F");		
+	});
+	$("#G").click(function(){
+			estar("G");		
+	});
+	$("#H").click(function(){
+			estar("H");		
+	});
+	$("#I").click(function(){
+			estar("I");		
+	});
+	$("#J").click(function(){
+			estar("J");		
+	});
+	$("#K").click(function(){
+			estar("K");		
+	});
+	$("#L").click(function(){
+			estar("L");		
+	});
+	$("#M").click(function(){
+			estar("M");		
+	});
+	$("#N").click(function(){
+			estar("N");		
+	});
+	$("#O").click(function(){
+			estar("O");		
+	});
+	$("#P").click(function(){
+			estar("P");		
+	});
+	$("#Q").click(function(){
+			estar("Q");		
+	});
+	$("#R").click(function(){
+			estar("R");		
+	});
+	$("#S").click(function(){
+			estar("S");		
+	});
+	$("#T").click(function(){
+			estar("T");		
+	});
+	$("#U").click(function(){
+			estar("U");		
+	});
+	 $("#V").click(function(){
+			estar("V");		
+	});
+	$("#W").click(function(){
+			estar("W");		
+	});
+	$("#X").click(function(){
+			estar("X");		
+	});
+	$("#Y").click(function(){
+			estar("Y");		
+	});
+	$("#Z").click(function(){
+			estar("Z");		
+	});
+});
